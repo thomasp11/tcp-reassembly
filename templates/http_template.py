dc27f44e16de8e2cfd1d96ddc878af7af3a5f4f2
Tom Peterson <thomasp11@gmail.com>
2019-03-12 22:32:27 -0400

    Add template for sending HTTP traffic
    This new template will send an HTTP GET request for / or a specific URL.
    It will process the first packet sent in response and then immediately
    close the connection so responses spanning multiple packets will not be
    processed correctly.

---
+++ templates/http_template.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: ascii -*-
+from scapy.all import *
+import argparse
+import time
+
+cmd_desc = 'Template for sending data with scapy over TCP'
+
+parser = argparse.ArgumentParser(description = cmd_desc)
+parser.add_argument("--url", help = "URL to GET | default = '/'", 
+                    type = str, nargs = '?', const = 1, default = '/')
+parser.add_argument("--host", help = "HTTP Host | default = 'www.example.com'", 
+                    type = str, nargs = '?', const = 1, default = 'www.example.com')
+parser.add_argument('ip', help = 'Destination IP Address')
+parser.add_argument('port', help = 'Destination TCP Port | default = 80',
+                    type = int, nargs = '?', const = 1, default = 80)
+args = parser.parse_args()
+
+# IP arguments
+dst_ip = args.ip
+
+# TCP arguments
+dst_port = args.port
+src_port = random.randint(1024,65535)
+tcp_seq = random.randint(0,4294967295)
+
+# HTTP arguments
+http_url = args.url
+http_host = args.host
+
+# HTTP header
+http_data = "GET " + http_url + " HTTP/1.1\r\nHost: " + http_host + "\r\n\r\n"
+
+# IP header
+ip = IP(dst = dst_ip)
+
+# SYN packet
+syn = TCP(sport = src_port, dport = dst_port, flags = 'S', seq = tcp_seq)
+tcp_seq = tcp_seq + 1
+
+# SYN-ACK response packet
+syn_ack_resp = sr1(ip/syn)
+tcp_ack = syn_ack_resp.seq
+
+tcp_ack = tcp_ack + 1
+
+# ACK packet
+ack = TCP(sport = src_port, dport = dst_port, flags = 'A', seq = tcp_seq, ack = tcp_ack)
+send(ip/ack)
+
+# Send stream
+http_tcp = TCP(sport = src_port, dport = dst_port, flags = 'A', seq = tcp_seq, ack = tcp_ack)
+http_ack = sr1(ip/http_tcp/http_data)
+tcp_seq = tcp_seq + len(http_data)
+
+tcp_ack = http_ack.ack
+
+
+# FIN packet
+fin = TCP(sport = src_port, dport = dst_port, flags = 'FA', seq = tcp_seq, ack = tcp_ack)
+fin_resp = sr1(ip/fin)
+
+tcp_seq = tcp_seq + 1
+tcp_ack = tcp_ack + 1
+ack = TCP(sport = src_port, dport = dst_port, flags = 'A', seq = tcp_seq, ack = tcp_ack)
+send(ip/ack)
